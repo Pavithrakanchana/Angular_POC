e89c9f2a04225eee9d92ab12a712d5a9bc88a864
pavi0405 <74247443+Pavithrakanchana@users.noreply.github.com>
2025-06-03 21:37:09 +0530

    Data binding concept completed.

---
+++ AngularPOC/src/app/concepts/data-binding/data-binding.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-data-binding',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './data-binding.component.html',
+  styleUrl: './data-binding.component.css'
+})
+export class DataBindingComponent {
+  
+text="hi,this is from interpolation";
+textValue='';
+
+onClick(event:any){
+  alert('Button Clicked! event binding happened');
+}
+}
