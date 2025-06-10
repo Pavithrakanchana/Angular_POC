f321d3cdec1a154f413edf77ca4a7c3347fc9b65
pavi0405 <74247443+Pavithrakanchana@users.noreply.github.com>
2025-06-10 16:46:59 +0530

    Attribute directives completed

--- AngularPOC/src/app/concepts/directives/directives.component.ts
+++ AngularPOC/src/app/concepts/directives/directives.component.ts
@@ -1,15 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-directives',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './directives.component.html',
   styleUrl: './directives.component.css'
 })
 export class DirectivesComponent {
   isInputNeeded:boolean = true;
   products:any = ["Iphone 14", "Samsung A51"];
+  isActive:boolean = true;
+  isHighlighted:boolean = true;
+  isItalic:boolean = true;
+  name : string = '';
 
 }
